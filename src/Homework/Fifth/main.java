c16eb8f4dd9f6a7b9578d7344aab7ec6b395598f
Stepan <stepanswim@gmail.com>
2022-03-28 00:49:10 +0300

    update

--- src/Homework/Fifth/main.java
+++ src/Homework/Fifth/main.java
@@ -17,17 +17,16 @@ public class main {
 
     public static void main(String[] args) throws IOException {
         createFile();
-//        writer();
+        writer();
         writeStream ();
 
         AppData appData = readToObject();
-
     }
 
     public static void createFile() {
         Random random = new Random();
 
-        for (int i = 0; i < 1000; i++ ) {
+        for (int i = 0; i < 100; i++ ) {
             fileObjectArrayList.add(new FileObject(i, random.nextInt(100), random.nextInt(1000) ));
         }
     }
